cc2c988651519fa6eb24d0d5e38224d388bdda21
Alex62510 <eagle625@tut.by>
2023-05-31 21:49:12 +0300

    commit 5 footer1

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import style from './Footer.module.css'
 import styleContainer from './../common/styles/Container.module.css'
-
 const Footer = () => {
     return (
         <div className={style.FooterBlock}>
